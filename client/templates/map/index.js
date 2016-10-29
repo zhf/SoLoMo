f57020149d356e6b534e68a815d6ac46f5cb56c6
crapthings <crapthings@gmail.com>
2016-10-30 00:25:53 +0800

    locate current position

--- client/templates/map/index.js
+++ client/templates/map/index.js
@@ -10,26 +10,15 @@ function baidu() {
 
   var baiduMap = 'http://api.map.baidu.com/getscript?v=2.0&ak=jDhQn9BmUIE0TaGA0oDheEi5w6Xmbr60'
 DocHead.loadScript(baiduMap, function() {
-  console.log(111)
 
-  $('map-container')
-
-      var map = new BMap.Map("map-container"); // 创建Map实例
-
-      var point = new BMap.Point(116.403694,39.927552);  // 创建点坐标
-  map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
-  map.enableScrollWheelZoom();
-  map.addControl(new BMap.NavigationControl());
-  // // map.centerAndZoom(new BMap.Point(116.404, 39.915), 11); // 初始化地图,设置中心点坐标和地图级别
-  // map.addControl(new BMap.MapTypeControl()); //添加地图类型控件
-  // map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
-
-  // map.setCurrentCity("哈尔滨");
-
-  // var local = new BMap.LocalSearch(map, {
-  //   renderOptions: { map: map }
-  // });
-  // local.search('轩辕东路');
+  navigator.geolocation.getCurrentPosition(function (pos) {
+    console.log(pos)
+    var map = new BMap.Map("map-container"); // 创建Map实例
+    var point = new BMap.Point(pos.coords.longitude, pos.coords.latitude);  // 创建点坐标
+    map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
+    map.enableScrollWheelZoom();
+    map.addControl(new BMap.NavigationControl());
+  })
 })
 
 
