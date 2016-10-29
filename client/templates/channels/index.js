7f3cb2b2e6519965cb63d3080648cbf145dafd53
zhf <hf@zhanghongfeng.com>
2016-10-29 23:32:05 +0800

    add styles to channel

--- client/templates/channels/index.js
+++ client/templates/channels/index.js
@@ -8,9 +8,13 @@ const data = {
   },
 }
 
-const index = () => <MeteorDataContainer sources={{ subscriptions, data, }} component={({ channels }) => <div className='flex flex-column'>
-  {channels.map(({ _id, name, ...channel }) => <a key={_id} className='flex flex-center-y pdlr list' href={`/channels/${name}`}>
-    {name}
+const index = () => <MeteorDataContainer sources={{ subscriptions, data, }} component={({ channels }) => <div className='channel-list flex flex-column'>
+  {channels.map(({ _id, name, logo, ...channel }) => <a key={_id} className='flex flex-center-y pdlr list' href={`/channels/${name}`}>
+    <div className="channel">
+      <img src={`/logos/${logo}`} />
+      {name}
+      <span className="FIXME">Members</span>
+    </div>
   </a>)}
 </div>} />
 
