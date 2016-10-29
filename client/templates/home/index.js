b7bedef88cd7614233a4d39798398c82ced88d22
crapthings <crapthings@gmail.com>
2016-10-29 15:06:04 +0800

    fake messages

--- client/templates/home/index.js
+++ client/templates/home/index.js
@@ -1,5 +1,13 @@
-const index = () => <div>
-  messages
-</div>
+const data = {
+  messages() {
+    return Messages.find().fetch()
+  }
+}
+
+const index = () => <MeteorDataContainer sources={{ data, }} component={({ messages }) => <div>
+  {messages.map(message => <p>
+    {message.text}
+  </p>)}
+</div>} />
 
 export default index
