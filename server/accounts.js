b8a59868b4a60a79032bedfdbfe5e56c1a51b7b5
crapthings <crapthings@gmail.com>
2016-10-29 22:50:57 +0800

    use channels file

--- server/accounts.js
+++ server/accounts.js
@@ -1,1 +1,4 @@
-Accounts.onCreateUser((option, user) => _.extend(user, option))
+Accounts.onCreateUser((option, user) => {
+  option.avatarUrl = faker.internet.avatar()
+  return _.extend(user, option)
+})
