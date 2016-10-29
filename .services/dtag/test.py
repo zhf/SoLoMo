23663f828fd3fd9450b36cfc0019409f2a7874c2
zhf <hf@zhanghongfeng.com>
2016-10-29 16:13:01 +0800

    add blacklist support

--- .services/dtag/test.py
+++ .services/dtag/test.py
@@ -4,12 +4,18 @@
 import jieba
 import sys
 from collections import Counter
+import re
 
 if len(sys.argv) == 1:
 	sys.exit("Missing argument")
 
-file = open(sys.argv[1],'r')
-text = file.read()
+input_file = open(sys.argv[1],'r')
+text = input_file.read()
+
+blacklist_file = open(sys.argv[2],'r')
+blacklist = re.split('[\s\ \\,\;\.\!\n]+', blacklist_file.read())
+
+print "黑名单", blacklist
 
 def minLength(x): return len(x) >= 2
 
@@ -21,7 +27,7 @@ print("Result: \n" + "\n".join(seg_list))
 def calcWordFreq(wordList, topN):
 	wordFreq = {}
 	for word in wordList:
-		if word in wordFreq:
+		if (word in wordFreq) and (not (word.encode('utf-8') in blacklist)):
 			wordFreq[word] = wordFreq[word] + 1
 		else:
 			wordFreq[word] = 1
